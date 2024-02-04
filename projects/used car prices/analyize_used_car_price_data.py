9b4466a749e8c289becdc53c92637897fb6ff2d1
morganhiggin2 <morganhigginbotham2@gmail.com>
2024-02-04 07:12:44 -0700

    machine learning projects added, most completed, deep learning projects planned

--- projects/used car prices/analyize_used_car_price_data.py
+++ projects/used car prices/analyize_used_car_price_data.py
@@ -18,3 +18,36 @@ each transition going between nodex (latent variable), with x, output, being the
 
 
 '''
+
+import numpy
+import matplotlib.pyplot as plot
+import matplotlib.colors
+from load_used_car_price_data import get_used_car_prices_dataset
+
+def price_classification(dataset): 
+    '''
+    lets find if there are 3 classes of cars based on selling price and mileage 
+    '''
+
+    from sklearn.cluster import KMeans
+
+    #get numpy arrays from data
+    X = dataset[['Kms_Driven', 'Selling_Price']].to_numpy()
+
+    #k means clustering
+    k_means_cluster = KMeans(n_clusters=3, random_state=0, n_init="auto", init="random").fit(X)
+
+    Y_predictions = k_means_cluster.predict(X)
+    color_dictionary = {0: 'green', 1: 'blue', 2: 'yellow'}
+    Y_prediction_color_labels = numpy.vectorize(color_dictionary.get)(Y_predictions)
+
+    plot.style.use('ggplot')
+    matplotlib.use('TkAgg')
+    plot.scatter(X[:,0], X[:,1], c=Y_predictions.tolist(), cmap=matplotlib.colors.ListedColormap(['blue', 'green', 'yellow']))
+    plot.xlabel("Selling Price")
+    plot.ylabel("Mileage (Kilometers)")
+    plot.show()
+    #Graph out class prediction for each training point, graph
+
+dataset = get_used_car_prices_dataset()
+price_classification(dataset)
--- projects/used car prices/load_used_car_price_data.py
+++ projects/used car prices/load_used_car_price_data.py
@@ -1,14 +1,18 @@
 import pandas
-import pyarrow
+import numpy
 import pyarrow.csv
 
 def get_used_car_prices_dataset():
-    raw_data = pyarrow.csv.read_csv('../data/used_car_prices/cardekho_data.csv')
+    dataset = pandas.read_csv('../data/used_car_prices/cardekho_data.csv')#pyarrow.csv.read_csv('../data/used_car_prices/cardekho_data.csv')
 
-    #convert pyarrow table to pandas, maps to existing data 
-    dataset = raw_data.to_pandas(split_blocks=True)
+    #encode Fuel_Type,Seller_Type,Transmission
+    fuel_type_class_labels = {label: i[0] for i, label in numpy.ndenumerate(dataset['Fuel_Type'].unique())}
+    dataset['Fuel_Type_Encoding'] = dataset['Fuel_Type'].apply(lambda label: fuel_type_class_labels[label])
+    seller_type_class_labels = {label: i[0] for i, label in numpy.ndenumerate(dataset['Seller_Type'].unique())}
+    dataset['Seller_Type_Encoding'] = dataset['Seller_Type'].apply(lambda label: seller_type_class_labels[label])
+    transmission_class_labels = {label: i[0] for i, label in numpy.ndenumerate(dataset['Transmission'].unique())}
+    dataset['Transmission_Encoding'] = dataset['Transmission'].apply(lambda label: transmission_class_labels[label])
 
-    print(dataset) 
-    print(dataset.dtypes)
+    return dataset
 
 get_used_car_prices_dataset()
