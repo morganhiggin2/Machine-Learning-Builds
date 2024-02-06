a6b08620cf8704f165051bba065bed48b6c088d8
morganhiggin2 <morganhigginbotham2@gmail.com>
2024-02-06 14:50:19 -0700

    gitignore update again

--- simple_linear_discriminant_analysis_2d.py
+++
@@ -1,9 +0,0 @@
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis  
-import numpy as np  
-import pandas     
-
-data = pandas.read_csv('data/simple_classifier_2d.py', index_col=False)  
-
-lda_machine = LinearDiscriminantAnalysis()                      
-lda_machine.fit(data[['x', 'y']].to_numpy(), data['t'].to_numpy())                                                                               
-print(lda_machine.predict([[3,1]]))
